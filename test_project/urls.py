2bde9c6333e6be80cbf4acc2f8681c9019534f28
kns003 <kns003@gmail.com>
2013-03-14 11:29:54 +0530

    Update urls.py
    password change,reset and static

--- test_project/urls.py
+++ test_project/urls.py
@@ -18,6 +18,21 @@ urlpatterns = patterns('',
     (r'^thread/delete/(?P<thread_id>\d+)/$',views.delete_thread),
     (r'^thread/edit/(?P<thread_id>\d+)/$',views.edit_thread),
     (r'^comment/edit/(?P<comment_id>\d+)/$',views.edit_comment),
+    (r'^user/password/reset/$','django.contrib.auth.views.password_reset'),    
+    (r'^user/password/reset/done/$','django.contrib.auth.views.password_reset_done'),
+    (r'^user/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$','django.contrib.auth.views.password_reset_confirm',
+    {'post_reset_redirect' : '/user/password/done/'}),
+    (r'^user/password/done/$','django.contrib.auth.views.password_reset_complete'),
+    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
+    (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
     
+    )
+    
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    'document_root': settings.MEDIA_ROOT,
+    }),
+)
 
 )
